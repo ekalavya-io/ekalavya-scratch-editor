cb04bc0b9e3453301bdb8bb00f819bc40d4a8dac
Tarun Shekhar <tarun.shekhar@ekalavya.io>
2025-10-06 11:02:16 +0000

    save project

--- packages/scratch-gui/src/lib/vm-manager-hoc.jsx
+++ packages/scratch-gui/src/lib/vm-manager-hoc.jsx
@@ -39,8 +39,8 @@ const vmManagerHOC = function (WrappedComponent) {
                 this.props.vm.start();
             }
 
-            // to load the project from parent app
-            window.addEventListener('message', event => {
+            // to load or save the project from parent app
+            window.addEventListener('message', async event => {
                 if (!event.data) return;
                 if (event.data.type === 'LOAD_PROJECT') {
                     const buffer = event.data.buffer;
@@ -50,7 +50,20 @@ const vmManagerHOC = function (WrappedComponent) {
                         })
                         .catch(e => console.error('Error loading project from buffer:', e));
                 }
+
+                if (event.data.type === 'SAVE_PROJECT') {
+                    const projectSb3 = await this.props.vm.saveProjectSb3();
+                    window.parent.postMessage(
+                        {
+                            type: 'PROJECT_SAVED',
+                            buffer: projectSb3,
+                            name: event.data.name || 'scratchProject.sb3'
+                        },
+                        '*'
+                    );
+                }
             });
+
         }
         componentDidUpdate (prevProps) {
             // if project is in loading state, AND fonts are loaded,
