eb7a0bab70861bbdc53a34e83a44d286be65d01a
Tarun Shekhar <tarun.shekhar@ekalavya.io>
2025-10-06 10:53:50 +0000

    update project

--- packages/scratch-gui/src/lib/vm-listener-hoc.jsx
+++ packages/scratch-gui/src/lib/vm-listener-hoc.jsx
@@ -74,6 +74,9 @@ const vmListenerHOC = function (WrappedComponent) {
             }
         }
         handleProjectChanged () {
+            if (typeof window !== 'undefined' && window.parent) {
+                window.parent.postMessage({type: 'PROJECT_CHANGED'}, '*');
+            }
             if (this.props.shouldUpdateProjectChanged && !this.props.projectChanged) {
                 this.props.onProjectChanged();
             }
